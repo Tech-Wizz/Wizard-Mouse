4762bd5436930e267ba89e8fad94ac3dde5288c8
Kruize Derek Christensen <christensen.kruize@gmail.com>
2023-11-22 10:36:30 -0700

    Changed Name

---
+++ Java/WizardMouse.java
@@ -0,0 +1,87 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class WizardMouse extends JFrame implements KeyListener {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            WizardMouse move = new WizardMouse();
+            move.setupUI();
+            new Thread(move::jiggleMouse).start();
+        });
+    }
+
+    private void setupUI() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(300, 200);
+        setLocationRelativeTo(null);
+
+        // Set custom icon
+        ImageIcon icon = new ImageIcon("/image/MouseWizard.png");
+        setIconImage(icon.getImage());
+
+        // Set image instead of text
+        ImageIcon image = new ImageIcon("image/MouseWizard.png");
+        JLabel imageLabel = new JLabel(image);
+        add(imageLabel);
+
+        // Set title
+        setTitle("Wizard Mouse");
+
+        addKeyListener(this);
+
+        setVisible(true);
+    }
+
+    public void jiggleMouse() {
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int screenWidth = (int) screenSize.getWidth();
+        int screenHeight = (int) screenSize.getHeight();
+
+        int radius = 20;
+        int centerX = screenWidth / 2;
+        int centerY = screenHeight / 2;
+
+        while (true) {
+            for (int angle = 0; angle < 360; angle += 10) {
+                int x = centerX + (int) (radius * Math.cos(Math.toRadians(angle)));
+                int y = centerY + (int) (radius * Math.sin(Math.toRadians(angle)));
+
+                moveMouse(x, y);
+
+                try {
+                    Thread.sleep(9000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public void moveMouse(int x, int y) {
+        try {
+            Robot robot = new Robot();
+            robot.mouseMove(x, y);
+        } catch (AWTException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // Unused, but required for KeyListener interface
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyChar() == 'M') {
+            System.exit(0);
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // Unused, but required for KeyListener interface
+    }
+}
